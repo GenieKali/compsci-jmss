22559f2e00948b51d40d93f2a24adf9db1c46fe9
GenieKali <kal0004@jmss.vic.edu.au>
2015-08-24 11:46:16 +1000

    Completed search code, used different method to find if the search number is not in the list

--- Coding/Linear Search.py
+++ Coding/Linear Search.py
@@ -4,13 +4,13 @@ list = [1,2,3,4,5,6,7,8,9,10]
 search = int(input("Enter a number: "))
 
 index = 0
+found = False
 
 for i in list:
     if i == search:
-        print ("The index of the number is:",index)
+        found = True
+        print ("Index of the number is", index)
     else:
         index += 1
-
-        
-if search not in list:
-    print ("Number not in list")
+if found == False:
+    print ("Number not found")
